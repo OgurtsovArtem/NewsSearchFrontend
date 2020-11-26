931918dff017082f85dba9e4d90b111e0f50f62a
Artem Ogurtsov <ogurcov.artem2012@list.ru>
2020-11-26 17:22:53 +0700

    Fixed code for easier viewing of elements

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ import './index.css';
 const buttonBurger = document.querySelector('.button__burger');
 const headerNav = document.querySelector('.header__nav');
 
-const buttonRegistration = document.querySelector('.popup__button-another-form');
+const buttonAnother = document.querySelectorAll('.popup__button-another-form');
 const buttonAuth = document.querySelector('.header__button-login');
 
 const popupLogin = document.querySelector('.popup-login');
 const popupReg = document.querySelector('.popup-registration');
 const successful = document.querySelector('.popup-successful');
-const popupClose = document.querySelector('.popup__close');
-const popupButton = document.querySelector('.popup__button');
+const popupClose = document.querySelectorAll('.popup__close');
+const popupButton = document.querySelector('.button__registration');
+
+const promt = document.querySelector('.place-card__prompt');
+const flag = document.querySelectorAll('.place-card__flag');
 
 popupButton.addEventListener('click', () => {
   successful.classList.remove('disabled');
   popupLogin.classList.add('disabled');
+  popupReg.classList.toggle('disabled');
 });
 
 buttonBurger.addEventListener('click', () => {
@@ -25,13 +29,35 @@ buttonBurger.addEventListener('click', () => {
 buttonAuth.addEventListener('click', () => {
   popupLogin.classList.remove('disabled');
   headerNav.classList.remove('header__nav_open');
-});
-buttonRegistration.addEventListener('click', () => {
-  popupReg.classList.toggle('disabled');
-  popupLogin.classList.add('disabled');
+  buttonBurger.classList.remove('active');
+  buttonBurger.classList.add('disabled');
 });
 
-popupClose.addEventListeners('click', () => {
-  popupLogin.classList.remove('disabled');
-  popupReg.classList.remove('disabled');
+//FORM CLOSE
+[].forEach.call(popupClose,function(el){
+  el.addEventListener('click', function () {
+    popupLogin.classList.add('disabled');
+    popupReg.classList.add('disabled');
+    successful.classList.add('disabled')
+    buttonBurger.classList.remove('disabled');
+  })
+});
+
+//TOOLTIP
+[].forEach.call(flag,function(el){
+  el.addEventListener('mouseout', function () {
+    promt.classList.remove('place-card__prompt_visible')
+  })
+  el.addEventListener('mouseover', function () {
+    promt.classList.add('place-card__prompt_visible')
+  })
+});
+
+//SWITCHING FORMS
+[].forEach.call(buttonAnother,function(el){
+  el.addEventListener('click', function () {
+    popupReg.classList.toggle('disabled');
+    popupLogin.classList.toggle('disabled');
+    successful.classList.add('disabled');
+  })
 });
--- src/pages/articles.js
+++ src/pages/articles.js
@@ -3,7 +3,33 @@ import './articles.css';
 const buttonBurger = document.querySelector('.button__burger');
 const headerNav = document.querySelector('.header__nav');
 
+const headerTitle = document.querySelector('.header__title');
+const headerButton = document.querySelector('.header__button');
+
+const backet = document.querySelectorAll('.place-card__backet');
+const promt = document.querySelector('.place-card__prompt');
+const keyword = document.querySelector('.place-card__keyword');
+
+
 buttonBurger.addEventListener('click', () => {
   buttonBurger.classList.toggle('active');
+  buttonBurger.classList.toggle('button_color-dark')
+  buttonBurger.classList.toggle('button_color-light')
+  headerTitle.classList.toggle('header__title_color-dark')
+  headerTitle.classList.toggle('header__title_color-light')
+  headerButton.classList.toggle('button_color-dark');
+  headerButton.classList.toggle('button_color-light');
   headerNav.classList.toggle('header__nav_open');
+});
+
+
+[].forEach.call(backet,function(el){
+  el.addEventListener('mouseout', function () {
+    promt.classList.remove('place-card__prompt_visible')
+    keyword.classList.remove('place-card__keyword_hide')
+  })
+  el.addEventListener('mouseover', function () {
+    promt.classList.add('place-card__prompt_visible')
+    keyword.classList.add('place-card__keyword_hide')
+  })
 });
